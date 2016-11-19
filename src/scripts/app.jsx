9d32360805676e5c5691cfa8eb787bf74420c315
Diego Rodríguez Suárez-Bustillo <diego@rodribus.com>
2016-11-19 18:49:10 +0100

    feat(magi): add initial setup files

---
+++ src/scripts/app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import style from '../styles/app.scss';
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<strong>ProjectMagi</strong>, app);
